e6c502c1068cd27be663ebe3cfc40ff5017d5c22
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-06-04 13:04:51 -0400

    finished further study

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import surveys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='password'
@@ -8,24 +8,38 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-questions = satisfaction_survey.questions
+# questions = satisfaction_survey.questions
 
 @app.route('/')
-def show_homepage():
-    title = satisfaction_survey.title
-    instructions = satisfaction_survey.instructions
+def choose_survey():
+    return render_template("choose_survey.html", surveys = surveys)
 
-    return render_template("homepage.html", title = title, instructions = instructions)
+@app.route('/', methods = ['POST'])
+def survey_session():
+    survey_name = request.form['survey']
+    survey = surveys[survey_name]
+    session['survey'] = survey_name
+
+    return render_template("homepage.html", survey = survey)
+
+# @app.route('/homepage')
+# def show_homepage():
+    
+#     # title = survey.title
+#     # instructions = survey.instructions
+
+#     return render_template("homepage.html", title = title, instructions = instructions)
 
 @app.route('/start')
 def start():
     session['responses'] = []
-    print(session['responses'])
     return redirect("/questions/0")
 
 @app.route('/questions/<int:q_number>')
 def show_questions(q_number):
     responses = session.get('responses')
+    survey_name = session['survey']
+    questions = surveys[survey_name].questions  
 
     if q_number >= len(questions):
         return redirect('/')
@@ -37,16 +51,22 @@ def show_questions(q_number):
     if len(responses) == len(questions):
         return redirect("/complete")
 
-    return render_template("questions.html", q_number = q_number, question = questions[q_number].question, choices = questions[q_number].choices)
+    return render_template("questions.html", allow_text = questions[q_number].allow_text, q_number = q_number, question = questions[q_number].question, choices = questions[q_number].choices)
 
 
 @app.route("/answer", methods = ["POST"])
 def add_answer():
     answer = request.form['answer']
+    comment = request.form.get('text', '')
     
+    survey_name = session['survey']
+    questions = surveys[survey_name].questions
+
     responses = session['responses']
-    responses.append(answer)
+    responses.append({"answer": answer, "comment": comment})
     session['responses'] = responses
+
+    
 
     if len(responses) == len(questions):
         return redirect("/complete")
@@ -55,6 +75,7 @@ def add_answer():
 
 @app.route("/complete")
 def completed_survery():
+    survey_name = session['survey']
     responses = session.get('responses')
-    return render_template("complete.html", responses=responses)
+    return render_template("complete.html", survey=surveys[survey_name], responses=responses)
     
